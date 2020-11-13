f378d43ec410c59b0c29ab5a48d417b569487312
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-12 16:25:19 -0800

    fixed about page

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -5,7 +5,6 @@ import backgroundImg from '../../Image/background.jpg';
 import aboutImg from '../../Image/screenshot/robeil.jpg';
 import Icons from '../Icon/index';
 
-
 class About extends React.Component {
     constructor(props) {
         super(props)
@@ -17,49 +16,45 @@ class About extends React.Component {
         return (
             <>
                 <Header />
-        
                 <div className="landingImg"
                     style={{
                         backgroundImage: 'url(' + backgroundImg + ')',
                         backgroundSize: "cover",
                         height: "100vh",
                         color: "#f5f5f5"
-
                     }}
                 >
-                    <div className="col-4 float-right top">
-                        <div className="font-style">
-                            <p>My name is Robeil Aregawi.
-                            I was born and raised in Eritrea.
-                            In 2017, I came to USA and I
-                            been working as
-                            professional driver and currently,
-                            am attending at University of Washington in Seattle as Full Stack Web
-                    Development.</p>
+                    <div className="card w-50 float-right">
+                        <div className="card-body">
+                            <div className="card-title">
+                                <div className="card-text">
+                                    <p>My name is Robeil Aregawi. I am full stack web developer</p>
+                                    <p>A graduate of the boot camp with an inclination towards front end development.
+                                    Strong professional graduated in November 2020 from University of Washington.
+                                    This professional history has embedded strong beliefs in teamwork,
+                                    critical thinking, and attention to detail within my core.
+                                    I am an extremely motivated person , enabling rapid professional growth and
+                                    solidifying essential connections in each workplace. Combined with my ability to learn quickly.
+                                Skilled in Customer Service, Professional Driving, Communication, Computer Hardware Installation. </p>
+                                    <div>
+                                        <ul className="navbar-nav text-dark">
+                                            <li><b>Full name</b> : Robeil Aregawi</li>
+                                            <li><b>Phone number</b> : 206-602-5164</li>
+                                            <li><b>Email address</b> : silukeen1@gmail.com</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="font-style">
                         <h1>About Me</h1>
                     </div>
                     <div className="aboutImg">
-                        <img style={{ borderRadius: "50%" }} src={aboutImg} alt="profile image" />
+                        <img style={{ borderRadius: "50%" }} src={aboutImg} alt="profile" />
                     </div>
                     <br>
                     </br>
-
-                    <div className="text-about ">
-                        <div className="col-sm-3 top float-right">
-                            <div className="card">
-                                <div className="card-body">
-                                    <ul className="navbar-nav text-dark">
-                                        <li><b>Full name</b> : Robeil Aregawi</li>
-                                        <li><b>Phone number</b> : 206-602-5164</li>
-                                        <li><b>Email address</b> : silukeen1@gmail.com</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <Icons />
                 </div>
             </>
