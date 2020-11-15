100e3eb5ac998d42b3cb3625f996bd04dc5b0520
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-15 01:23:03 -0800

    about me page is fixed

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -4,8 +4,8 @@ import './style.css';
 import backgroundImg from '../../Image/background.jpg';
 import Icon from '../Icon/index';
 import CovidImg from '../../Image/screenshot/CovidTracker.png';
-import Jobimg from '../../Image/screenshot/JobFinder.png';
-import DayImg from '../../Image/screenshot/DayPlanner.png';
+//import Jobimg from '../../Image/screenshot/JobFinder.png';
+//import DayImg from '../../Image/screenshot/DayPlanner.png';
 
 const Portfolio = () => {
 
@@ -23,7 +23,7 @@ const Portfolio = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-4">
-                            <div className="card text-white bg-dark">
+                            <div className="card project-1 text-white bg-dark">
                                 <img src={CovidImg} className="card-img-top" alt="covid img" />
                                 <div class="card-body">
                                     <h4 class="card-title">Covid Tracker</h4>
@@ -31,32 +31,6 @@ const Portfolio = () => {
                                     <br>
                                     </br>
                                     <a href="https://sheplt1.github.io/covid-tracker/">Deployed link</a>
-                                    <p class="card-text"></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="card text-white bg-dark">
-                                <img src={Jobimg} className="card-img-top" alt="job finder img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Job Finder</h4>
-                                    <a href="https://github.com/saikitjk/JobFinder.git">GitHub Repo</a>
-                                    <br>
-                                    </br>
-                                    <a href="https://uw-jobfinder.herokuapp.com/">Deployed link</a>
-                                    <p class="card-text"></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="card text-white bg-dark">
-                                <img src={DayImg} className="card-img-top" alt="day planner img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Day Planner</h4>
-                                    <a href="https://github.com/Robeil/day-planner.git">GitHub Repo</a>
-                                    <br>
-                                    </br>
-                                    <a href="https://robeil.github.io/day-planner/">Deployed link</a>
                                     <p class="card-text"></p>
                                 </div>
                             </div>
